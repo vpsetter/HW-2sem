8305a892d87b650ac0aec5bb3322f19eda5e99e5
vpsetter <timurfakhrutdinov@gmail.com>
2021-03-16 18:39:10 +0300

    fixed 6.1 solution

--- HW 6 (18)/6.1.cpp
+++ HW 6 (18)/6.1.cpp
@@ -5,56 +5,55 @@
 
 int main()
 {
-	std::ifstream fin("test.txt", std::ios::in | std::ios::binary);
-	std::ofstream fout("output 6.1.txt", std::ios::out | std::ios::trunc | std::ios::binary);
+	std::fstream fin("test.txt", std::ios::in | std::ios::binary);
+	std::fstream fout("output 6.1.txt", std::ios::out | std::ios::trunc | std::ios::binary);
 
 	std::istreambuf_iterator < char > istreambuf_iterator(fin);
 	std::istreambuf_iterator < char > end;
 	std::ostreambuf_iterator < char > ostreambuf_iterator(fout);
 
-	int n = 0;
-
-	char current_symbol = 'a';
-
 	while (istreambuf_iterator != end)
 	{
-		while ((istreambuf_iterator != end) && (*istreambuf_iterator != '/') && (*istreambuf_iterator != '"'))
-		{
-			*ostreambuf_iterator++ = *istreambuf_iterator++;
-		}
+		for (; (istreambuf_iterator != end) && (*istreambuf_iterator != '/') && (*istreambuf_iterator != '"');
+			*ostreambuf_iterator++ = *istreambuf_iterator++);
+
 		if ((istreambuf_iterator != end) && (*istreambuf_iterator == '"'))
 		{
-			char prev_symbol = *istreambuf_iterator;
+			auto prev_symbol = *istreambuf_iterator;
 			*ostreambuf_iterator++ = *istreambuf_iterator++;
-			while ((istreambuf_iterator != end) && ((*istreambuf_iterator != '"') || (prev_symbol == '\\')))
-			{
-				prev_symbol = *istreambuf_iterator;
-				*ostreambuf_iterator++ = *istreambuf_iterator++;		
-			}
+
+			for (; (istreambuf_iterator != end) && ((*istreambuf_iterator != '"') || (prev_symbol == '\\'));
+				prev_symbol = *istreambuf_iterator, *ostreambuf_iterator++ = *istreambuf_iterator++);
+
 			*ostreambuf_iterator++ = *istreambuf_iterator++;
 		}
+
 		if ((istreambuf_iterator != end) && (*istreambuf_iterator == '/'))
 		{
-			++istreambuf_iterator;
-			if ((istreambuf_iterator != end) && (*istreambuf_iterator == '*'))
+			auto symbol = *istreambuf_iterator++;
+			if (istreambuf_iterator != end)
 			{
-				++istreambuf_iterator;
-				for (; (istreambuf_iterator != end) && (*istreambuf_iterator != '*'); ++istreambuf_iterator);
-				for (auto i = 0; (i < 2) && (istreambuf_iterator != end); ++i, ++istreambuf_iterator);
-			}
-			else
-			{
-				for (; (istreambuf_iterator != end) && (*istreambuf_iterator != '\n') && (*istreambuf_iterator != '\r'); ++istreambuf_iterator);
-				if (istreambuf_iterator != end)
+				switch (*istreambuf_iterator)
 				{
-					*ostreambuf_iterator++ = *istreambuf_iterator++;
+				case '*':
+					++istreambuf_iterator;
+					for (; (istreambuf_iterator != end) && (*istreambuf_iterator != '*'); ++istreambuf_iterator);
+					for (auto i = 0; (i < 2) && (istreambuf_iterator != end); ++i, ++istreambuf_iterator);
+					break;
+				case '/':
+					for (; (istreambuf_iterator != end) && (*istreambuf_iterator != '\n') && (*istreambuf_iterator != '\r'); ++istreambuf_iterator);
+					if (istreambuf_iterator != end)
+					{
+						*ostreambuf_iterator++ = *istreambuf_iterator++;
+					}
+					break;
+				default:
+					*ostreambuf_iterator++ = symbol;
+					break;
 				}
 			}
 		}
 	}
-
-	fin.close();
-	fout.close();
 
 	return 0;
 }

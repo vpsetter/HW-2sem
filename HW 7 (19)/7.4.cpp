5b4c828724854d003543c78ba834ce0da2b7faa6
vpsetter <timurfakhrutdinov@gmail.com>
2021-03-25 23:14:37 +0300

    fixed 7.4 solution

--- HW 7 (19)/7.4.cpp
+++ HW 7 (19)/7.4.cpp
@@ -4,6 +4,7 @@
 #include <numeric>
 #include <chrono>
 #include <execution>
+#include <functional>
 
 #include "Timer.hpp"
 
@@ -11,9 +12,14 @@ int main()
 {
 	const std::size_t size = 10'000'000U;
 
-	std::vector <int> v(size, 0);
-	std::vector <long long> results(size, 0ll);
-	std::iota(std::begin(v), std::end(v), 0);
+	std::vector <double> v(size, 0.0);
+	std::vector <double> results(size, 1.0);
+	std::iota(std::begin(v), std::end(v), 1.0);
+
+	auto result1 = 0.0;
+	auto result2 = 0.0;
+
+	auto binary_op = [](double d1, double d2) { return std::sqrt(d1 * d1 + d2 * d2); };
 
 	std::cout << "size of vector: " << size << '\n';
 
@@ -29,23 +35,25 @@ int main()
 
 	{
 		Timer<std::chrono::microseconds> sequential_time("sequential partial sum time");
-		std::partial_sum(std::begin(v), std::end(v), std::begin(results));
+		std::partial_sum(std::begin(v), std::end(v), std::begin(results), binary_op);
 	}
 
 	{
 		Timer<std::chrono::microseconds> concurrent_time("concurrent partial sum (inclusive scan) time");
-		std::inclusive_scan(std::execution::par, std::begin(v), std::end(v), std::begin(results));
+		std::inclusive_scan(std::execution::par, std::begin(v), std::end(v), std::begin(results), binary_op);
 	}
 
 	{
 		Timer<std::chrono::microseconds> sequential_time("sequential inner product time");
-		auto result = std::inner_product(std::begin(v), std::end(v), std::begin(results), 0);
+		result1 = std::inner_product(std::begin(v), std::end(v), std::begin(results), 1.0);
 	}
 
 	{
 		Timer<std::chrono::microseconds> concurrent_time("concurrent inner product (transform reduce) time");
-		auto result = std::transform_reduce(std::execution::par, std::begin(v), std::end(v), std::begin(results), 0);
+		result2 = std::transform_reduce(std::execution::par, std::begin(v), std::end(v), std::begin(results), 1.0);
 	}
+
+	std::cout << "inner_product result = " << result1 << " , transform_reduce result = " << result2 << '\n';
 
 	return 0;
 }

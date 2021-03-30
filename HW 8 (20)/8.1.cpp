557b88e71bf7d00fbc05e1d7e39ba7e4315989b6
vpsetter <timurfakhrutdinov@gmail.com>
2021-03-30 10:22:41 +0300

    added 8.1 solution

---
+++ HW 8 (20)/8.1.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <random>
+#include <chrono>
+#include <vector>
+#include <numeric>
+#include <iterator>
+#include <thread>
+#include <functional>
+#include <future>
+#include <algorithm>
+
+class Threads_Guard
+{
+public:
+	explicit Threads_Guard(std::vector < std::thread >& threads) : m_threads(threads)
+	{}
+
+	Threads_Guard(Threads_Guard const&) = delete;
+
+	Threads_Guard& operator=(Threads_Guard const&) = delete;
+
+	~Threads_Guard() noexcept
+	{
+		try
+		{
+			for (std::size_t i = 0; i < m_threads.size(); ++i)
+			{
+				if (m_threads[i].joinable())
+				{
+					m_threads[i].join();
+				}
+			}
+		}
+		catch (...)
+		{
+			std::cerr << "\nDTOR THREAD ERROR\n";
+		}
+	}
+
+private:
+	std::vector < std::thread >& m_threads;
+};
+
+template < typename F, typename Distr_t>
+struct count_points
+{
+	void operator()(const std::size_t N, F predicate, Distr_t distribution, std::atomic<std::size_t> &counter)
+	{
+		std::mt19937_64 generator(std::hash<std::thread::id>{}(std::this_thread::get_id()));
+		std::pair <double, double> point = { 0.0, 0.0 };
+		for (auto i = 0U; i < N; ++i)
+		{
+			point.first = distribution(generator);
+			point.second = distribution(generator);
+			if (predicate(point))
+			{
+				++counter;
+			}
+		}
+	}
+};
+
+template <typename F, typename Distr_t >
+std::size_t parallel_count(const std::size_t points_num, F predicate, Distr_t distribution)
+{
+	const std::size_t hardware_threads = std::thread::hardware_concurrency();
+	const std::size_t num_threads = (hardware_threads != 0U ? hardware_threads : 2U);
+	const std::size_t block_size = points_num / num_threads;
+
+	std::vector < std::future < void > > futures(num_threads - 1U);
+	std::vector < std::thread > threads(num_threads - 1U);
+
+	Threads_Guard guard(threads);
+
+	std::atomic<std::size_t> counter = 0U;
+
+	for (std::size_t i = 0U; i < (num_threads - 1U); ++i)
+	{
+		std::packaged_task < void(const std::size_t, F, Distr_t, std::atomic<std::size_t>&) > task{ count_points <F, Distr_t>() };
+
+		futures[i] = task.get_future();
+		threads[i] = std::thread(std::move(task), block_size, predicate, distribution, std::ref(counter));
+	}
+
+	count_points < F, Distr_t >()(points_num - (num_threads - 1U) * block_size, predicate, distribution, counter);
+
+	for (std::size_t i = 0U; i < (num_threads - 1U); ++i)
+	{
+		futures[i].get();
+	}
+
+	return counter;
+}
+
+int main()
+{
+	const double radius = 1.0;
+	const std::size_t points_num = 80'000'000U;
+
+	std::uniform_real_distribution <> urd(0.0, radius);
+
+	auto lambda = [](std::pair<double, double>& point) {return (point.first * point.first + point.second * point.second < 1.0); };
+
+	auto inside = parallel_count(points_num, lambda, urd);
+
+	double pi = 4.0 * radius * radius * inside / points_num;
+	std::cout << "pi = " << pi << '\n';
+
+	return 0;
+}

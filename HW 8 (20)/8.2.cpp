3277a81ccdd5df14deb663bec9ca851af0667c74
vpsetter <timurfakhrutdinov@gmail.com>
2021-04-01 23:26:06 +0300

    fixed 8.2 solution

--- HW 8 (20)/8.2.cpp
+++ HW 8 (20)/8.2.cpp
@@ -6,35 +6,38 @@
 #include <iostream>
 #include <iomanip>
 
-std::mutex mutex;
-
 template<typename T>
-void safe_push_back(std::vector<T>& vector, T value)
+void safe_push_back(std::vector<T>& vector, T value, std::mutex& mutex)
 {
-	std::lock_guard < std::mutex > lock(mutex);
+	std::scoped_lock lock(mutex);
 	vector.push_back(value);
 }
 
 struct find_substr_block
 {
 	void operator()(const std::string & string_to_find, const std::string & string,
-		std::size_t first, std::size_t last, std::vector<std::size_t>& indices)
+		std::size_t first, std::size_t last, std::vector<std::size_t>& indices, std::mutex& mutex)
 	{
 		auto length = string_to_find.size();
-		auto begin = std::next(std::begin(string), first);
-		auto end = std::next(begin, length - 1);
+		auto begin = std::begin(string);
+		auto substr_begin = std::begin(string_to_find);
+		auto substr_end = std::end(string_to_find);
 		
 		for (auto index = first; index < last; ++index)
 		{
-			if (string_to_find == std::string(begin++, ++end))
+			auto it1 = substr_begin;
+			auto it2 = std::next(begin, index);
+			for (; it1 != substr_end && *it1 == *it2; ++it1, ++it2);
+			if (it1 == substr_end)
 			{
-				safe_push_back(indices, index);
+				safe_push_back(indices, index, mutex);
 			}
 		}
 	}
 };
 
-void parallel_find_substr(const std::string & string_to_find, const std::string & string, std::size_t first, std::size_t last, std::vector<std::size_t>& indices)
+void parallel_find_substr(const std::string & string_to_find, const std::string & string,
+	std::size_t first, std::size_t last, std::vector<std::size_t>& indices, std::mutex& mutex)
 {
 	auto substring_length = string_to_find.size();
 	auto string_length = string.size();
@@ -43,15 +46,16 @@ void parallel_find_substr(const std::string & string_to_find, const std::string 
 
 	if ((last - first) <= std::max(16U, (string_length - substring_length + 1) / hardware_threads ))
 	{
-		find_substr_block()(string_to_find, string, first, last, indices);
+		find_substr_block()(string_to_find, string, first, last, indices, mutex);
 	}
 	else
 	{
 		auto middle = first + (last - first) / 2;
 
-		std::future < void > first_half = std::async(parallel_find_substr, std::cref(string_to_find), std::cref(string), first, middle, std::ref(indices));
+		std::future < void > first_half = std::async(parallel_find_substr, std::cref(string_to_find), std::cref(string),
+			first, middle, std::ref(indices), std::ref(mutex));
 
-		find_substr_block()(string_to_find, string, middle, last, indices);
+		parallel_find_substr(string_to_find, string, middle, last, indices, mutex);
 
 		first_half.get();
 	}
@@ -59,6 +63,8 @@ void parallel_find_substr(const std::string & string_to_find, const std::string 
 
 int main()
 {
+	std::mutex mutex;
+
 	const std::string string = "AGCTTTTGCTAAATGCGGCTTAGGTGTCAGTGGTTTCGAGTCGTGGGTTTAAAATGTGTCCCGTTGAAAGTGGGGCCTTGAAATTCGTAGT";
 	std::cout << "String: " << string << '\n' << "Enter a substring\n";
 	std::string substring = "";
@@ -66,7 +72,7 @@ int main()
 
 	std::vector <std::size_t> indices;
 
-	parallel_find_substr(substring, string, 0U, string.size() - substring.size() + 1, indices);
+	parallel_find_substr(substring, string, 0U, string.size() - substring.size() + 1, indices, mutex);
 
 	std::sort(std::begin(indices), std::end(indices));
 

dd77c875c8a5a9007002f8264203ee0b95c084b6
vpsetter <timurfakhrutdinov@gmail.com>
2021-04-29 12:59:52 +0300

    improved 12.3 solution

--- HW 12 (24)/12.3.cpp
+++ HW 12 (24)/12.3.cpp
@@ -74,10 +74,9 @@ private:
 public:
 	Visualizer_Brownian_Motion() :
 		m_window(VideoMode(m_width, m_height), m_window_name),
-		m_uid(1,4), m_bd(0.1),
+		m_uid(0,3), m_bd(0.1),
 		m_field(m_width, std::vector<atomic_wrapper<std::size_t>>(m_height, 0U))
 	{
-		m_image.create(m_width, m_height);
 		std::uniform_int_distribution uid(0U, m_width - 1);
 		for (auto i = 0U; i < m_particles_num; ++i)
 		{
@@ -85,35 +84,52 @@ public:
 			auto y = uid(m_mt);
 			auto dir = static_cast<Direction>(m_uid(m_mt));
 			++m_field[x][y];
-			m_particles.push_back(Particle( x, y, dir));
+			m_particles.push_back(Particle(x, y, dir));
 		}
 	};
 
 	~Visualizer_Brownian_Motion() noexcept = default;
 
 private:
+	std::size_t square_count(std::size_t i, std::size_t j)
+	{
+		std::atomic<std::size_t> result = 0U;
+		auto begin_x = std::next(std::begin(m_field), i * m_squares_size);
+		auto end_x = std::next(begin_x, m_squares_size);
+		std::for_each(std::execution::par, begin_x, end_x, [this, j, &result](auto& column)
+			{
+				auto begin_y = std::next(std::begin(column), j * m_squares_size);
+				auto end_y = std::next(begin_y, m_squares_size);
+
+				result += std::accumulate(begin_y, end_y, 0U);
+			});
+		return result;
+	}
+
 	void draw()
 	{
-		for (auto i = 0U; i < m_width; ++i)
+		RectangleShape rect(Vector2f(m_squares_size, m_squares_size));
+		for (auto i = 0U; i < m_squares_in_column; ++i)
 		{
-			for (auto j = 0U; j < m_height; ++j)
+			for (auto j = 0U; j < m_squares_in_column; ++j)
 			{
-				const std::size_t particles_num = m_field[i][j];
+				Color color;
+				const std::size_t particles_num = square_count(i,j);
 				if (!particles_num)
 				{
-					m_image.setPixel(i, j, Color::Black);
+					color = Color::Black;
 				}
 				else
 				{
-					auto saturation = particles_num / change_color_particles_num;
-					saturation = (saturation > colors_num - 1 ? colors_num - 1 : saturation);
-					m_image.setPixel(i, j, m_colors[saturation]);
+					auto saturation = particles_num / m_change_color_particles_num;
+					saturation = (saturation > m_colors_num - 1 ? m_colors_num - 1 : saturation);
+					color = m_colors[saturation];
 				}
+				rect.setPosition(static_cast <float> (i * m_squares_size), static_cast <float> (j * m_squares_size));
+				rect.setFillColor(color);
+				m_window.draw(rect);
 			}
 		}
-		m_texture.loadFromImage(m_image);
-		m_sprite.setTexture(m_texture, true);
-		m_window.draw(m_sprite);
 		m_window.display();
 	}
 
@@ -211,21 +227,21 @@ private:
 
 	inline static const std::size_t m_width = 1000U;
 	inline static const std::size_t m_height = 1000U;
-	inline static const std::size_t m_particles_num = 10'000'000U;
+	inline static const std::size_t m_particles_num = 1000U;
 
 	std::vector<Particle> m_particles;
 	std::vector<std::vector<atomic_wrapper<std::size_t>>> m_field;
 
 	const std::string m_window_name = "Brownian motion";
 	RenderWindow m_window;
-	Image m_image;
-	Sprite m_sprite;
-	Texture m_texture;
 
-	inline static const std::size_t colors_num = 5U;
-	const std::array<Color, colors_num> m_colors = 
+	inline static const std::size_t m_colors_num = 5U;
+	const std::array<Color, m_colors_num> m_colors = 
 	{Color(50, 0, 0), Color(100, 0, 0), Color(150, 0, 0), Color(200, 0, 0), Color(250, 0, 0)};
-	const std::size_t change_color_particles_num = 5U;
+	inline static const std::size_t m_change_color_particles_num = 5U;
+
+	inline static const std::size_t m_squares_in_column = 10U;
+	inline static const std::size_t m_squares_size = m_height / m_squares_in_column;
 };
 
 int main()
